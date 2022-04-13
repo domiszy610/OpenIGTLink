b6ce5501baa7c5b057ee85016a952ab94d7c6f9e
domiszy610 <student@ib.polsl.pl>
2022-04-13 11:05:23 +0200

    Changed NowyKlient

--- Examples/Lab_PWP/NowyKlient.cxx
+++ Examples/Lab_PWP/NowyKlient.cxx
@@ -101,10 +101,109 @@ int main(int argc, char* argv[])
   // Send
   socket->Send(pointMsg->GetPackPointer(), pointMsg->GetPackSize());
   
+  while(1){
+	  socket = serverSocket->WaitForConnection(1000);
+    
+    if (socket.IsNotNull()) // if client connected
+      {
+      // Create a message buffer to receive header
+      igtl::MessageHeader::Pointer headerMsg;
+      headerMsg = igtl::MessageHeader::New();
+
+	   //------------------------------------------------------------
+      // Allocate a time stamp 
+      igtl::TimeStamp::Pointer ts;
+      ts = igtl::TimeStamp::New();
+
+      //------------------------------------------------------------
+      // loop
+      for (int i = 0; i < 100; i ++)
+        {
+
+        // Initialize receive buffer
+        headerMsg->InitPack();
+
+        // Receive generic header from the socket
+        int r = socket->Receive(headerMsg->GetPackPointer(), headerMsg->GetPackSize());
+        if (r == 0)
+          {
+          socket->CloseSocket();
+          }
+        if (r != headerMsg->GetPackSize())
+          {
+          continue;
+          }
+
+        // Deserialize the header
+        headerMsg->Unpack();
+
+        // Get time stamp
+        igtlUint32 sec;
+        igtlUint32 nanosec;
+        
+        headerMsg->GetTimeStamp(ts);
+        ts->GetTimeStamp(&sec, &nanosec);
+
+        std::cerr << "Time stamp: "
+                  << sec << "." << std::setw(9) << std::setfill('0') 
+                  << nanosec << std::endl;
+
+
+	  ReceivePoint(socket, headerMsg);
+
+  }
   
   //------------------------------------------------------------
   // Close the socket
   socket->CloseSocket();
 
+
 }
+
+  int ReceivePoint(igtl::Socket * socket, igtl::MessageHeader * header)
+{
+
+  std::cerr << "Receiving POINT data type." << std::endl;
+
+  // Create a message buffer to receive transform data
+  igtl::PointMessage::Pointer pointMsg;
+  pointMsg = igtl::PointMessage::New();
+  pointMsg->SetMessageHeader(header);
+  pointMsg->AllocatePack();
+
+  // Receive transform data from the socket
+  socket->Receive(pointMsg->GetPackBodyPointer(), pointMsg->GetPackBodySize());
+
+  // Deserialize the transform data
+  // If you want to skip CRC check, call Unpack() without argument.
+  int c = pointMsg->Unpack(1);
+
+  if (c & igtl::MessageHeader::UNPACK_BODY) // if CRC check is OK
+    {
+    int nElements = pointMsg->GetNumberOfPointElement();
+    for (int i = 0; i < nElements; i ++)
+      {
+      igtl::PointElement::Pointer pointElement;
+      pointMsg->GetPointElement(i, pointElement);
+
+      igtlUint8 rgba[4];
+      pointElement->GetRGBA(rgba);
+
+      igtlFloat32 pos[3];
+      pointElement->GetPosition(pos);
+
+      std::cerr << "========== Element #" << i << " ==========" << std::endl;
+      std::cerr << " Name      : " << pointElement->GetName() << std::endl;
+      std::cerr << " GroupName : " << pointElement->GetGroupName() << std::endl;
+      std::cerr << " RGBA      : ( " << (int)rgba[0] << ", " << (int)rgba[1] << ", " << (int)rgba[2] << ", " << (int)rgba[3] << " )" << std::endl;
+      std::cerr << " Position  : ( " << std::fixed << pos[0] << ", " << pos[1] << ", " << pos[2] << " )" << std::endl;
+      std::cerr << " Radius    : " << std::fixed << pointElement->GetRadius() << std::endl;
+      std::cerr << " Owner     : " << pointElement->GetOwner() << std::endl;
+      std::cerr << "================================" << std::endl;
+      }
+    }
+
+  return 1;
+}
+
 
